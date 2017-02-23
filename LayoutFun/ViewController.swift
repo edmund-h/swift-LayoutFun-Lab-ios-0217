ac72795629e6a77e16d261ac6be45ce2d5802185
edmund-h <447ght@gmail.com>
2017-02-23 10:19:21 -0500

    RAINBOW SKYSCRAPER <3

--- LayoutFun/ViewController.swift
+++ LayoutFun/ViewController.swift
@@ -32,8 +32,30 @@ extension ViewController {
     
     func setupTheConstraints() {
         // TODO: Create all the constraints (in code) for the various views
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive=true
+        redView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive=true
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor, multiplier: 1).isActive=true
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
         
+        orangeView.bottomAnchor.constraint(equalTo: redView.topAnchor).isActive=true
+        orangeView.widthAnchor.constraint(equalTo: redView.widthAnchor, multiplier: 0.75).isActive=true
+        orangeView.heightAnchor.constraint(equalTo: orangeView.widthAnchor, multiplier: 1).isActive=true
+        orangeView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
         
+        yellowView.bottomAnchor.constraint(equalTo: orangeView.topAnchor).isActive=true
+        yellowView.widthAnchor.constraint(equalTo: orangeView.widthAnchor, multiplier: 0.75).isActive=true
+        yellowView.heightAnchor.constraint(equalTo: yellowView.widthAnchor, multiplier: 1).isActive=true
+        yellowView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
+        
+        greenView.bottomAnchor.constraint(equalTo: yellowView.topAnchor).isActive=true
+        greenView.widthAnchor.constraint(equalTo: yellowView.widthAnchor, multiplier: 0.75).isActive=true
+        greenView.heightAnchor.constraint(equalTo: greenView.widthAnchor, multiplier: 1).isActive=true
+        greenView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
+        
+        blueView.bottomAnchor.constraint(equalTo: greenView.topAnchor).isActive=true
+        blueView.widthAnchor.constraint(equalTo: greenView.widthAnchor, multiplier: 0.75).isActive=true
+        blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor, multiplier: 1).isActive=true
+        blueView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
     }
     
     func turnOffAutoResizingOnAllViews() {
